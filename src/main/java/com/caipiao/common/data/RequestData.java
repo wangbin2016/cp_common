1385c3cb10e946bd753cc0730c128c076b42bc43
wangb <wangb@DESKTOP-F98CAL0>
2018-03-22 18:06:03 +0800

    调整实体

--- src/main/java/com/caipiao/common/data/RequestData.java
+++ src/main/java/com/caipiao/common/data/RequestData.java
@@ -3,13 +3,13 @@ package com.caipiao.common.data;
 import com.caipiao.common.data.body.DataBody;
 
 /**
- * 请求接口实体
+ * 请求接口实体  目前未使用
  * @author wangb
  *
  * @param <T>
  */
 @lombok.Data
-public class RequestData<T extends DataBody> {
+public class RequestData<T extends DataBody>  {
 	/**
 	 * 请求的token
 	 */
