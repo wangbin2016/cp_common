f2958cee478afe4022b166464326a0aec2959944
WangB <wxjwb@qq.com>
2018-04-02 17:53:16 +0800

    add status

--- src/main/java/com/caipiao/common/data/responsestatus/MemberResponseStatus.java
+++ src/main/java/com/caipiao/common/data/responsestatus/MemberResponseStatus.java
@@ -12,4 +12,6 @@ public class MemberResponseStatus extends AbstractResponseStatus{
 	}
 	public static MemberResponseStatus NOT_LOGIN = new MemberResponseStatus("100001", "用户未登录");
 	public static MemberResponseStatus LOGIN_ERROR = new MemberResponseStatus("100002", "帐号或者密码错误");
+	public static MemberResponseStatus LOGIN_INFO_ERROR = new MemberResponseStatus("100003", "用户登录信息有误");
+	public static MemberResponseStatus MEMBER_LOCK_ERROR = new MemberResponseStatus("100004", "帐号被锁定,请联系客户");
 }
