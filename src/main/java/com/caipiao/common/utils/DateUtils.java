a432821ac9a4ff634fb964540beafa2ae6790663
WangB <wxjwb@qq.com>
2018-05-15 17:40:43 +0800

    add utils

---
+++ src/main/java/com/caipiao/common/utils/DateUtils.java
@@ -0,0 +1,33 @@
+package com.caipiao.common.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class DateUtils {
+	/**
+	 * 日期格式:yy-MM-dd HH:mm
+	 */
+	public static final String yyMMdd_HHmm="yy-MM-dd HH:mm";
+	/**
+	 * 日期格式:yy-MM-dd HH:mm:ss
+	 */
+	public static final String yyMMdd_HHmmss="yy-MM-dd HH:mm:ss";
+	/**
+	 * 日期格式:yyyy-MM-dd HH:mm:ss
+	 */
+	public static final String yyyyMMdd_HHmmss="yyyy-MM-dd HH:mm:ss";
+	public static Date StringToDate(String dateString, String format) {
+		Date date;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat(format);
+			date = sdf.parse(dateString);
+		} catch (Exception e) {
+			log.info("日期格式失败");
+			date = null;
+		}
+
+		return date;
+	}
+}
